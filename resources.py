fbc1dcd48a4763383297ee27f1bf7a3cd9ee81bd
Thisislearner <ak97plutonium@gmail.com>
2022-11-20 21:11:12 +0530

    resources
    created the resources file.

---
+++ resources.py
@@ -0,0 +1,24 @@
+########################################################
+# THIS CONTAINS ALL THE RESOURCES FOR COFFEE AND SNACKS.
+########################################################
+
+main_ingredients = {
+    "coffee": 240,
+    "milk": 240,
+    "water": 240,
+    "tea": 240
+}
+
+sweeteners = {
+    "sugar": 100,
+    "s_free": 100,
+    "honey": 100
+}
+
+snacks = {
+    "cookie": 100,
+    "toast": 100,
+    "biscuit": 100,
+    "patties": 100,
+    "cakes": 100
+}
