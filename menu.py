c3e2f44a24a762ad4f58b42d88ca55bde5c95a44
Thisislearner <ak97plutonium@gmail.com>
2022-11-20 21:24:48 +0530

    menu Created
    created menu with all drinks names in it.

---
+++ menu.py
@@ -0,0 +1,30 @@
+#######################################################
+# THIS IS THE MENU TABLE. it only holds the menu chart.
+#######################################################
+
+MENU = {
+    "latte": {
+        "water": 15,
+        "coffee": 20,
+        "milk": 20
+    },
+    "cappuccino": {
+        "milk": 20,
+        "coffee": 20,
+        "cream": 15,
+    },
+    "espresso": {
+        "water": 15,
+        "milk": 25,
+        "coffee": 20
+    },
+    "blackCoffee": {
+        "coffee": 30,
+        "water": 15
+    },
+    "tea": {
+        "milk": 15,
+        "tea": 10,
+        "water": 10
+    }
+}
