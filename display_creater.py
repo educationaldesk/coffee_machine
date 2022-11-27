868f76dcb8ebca261c9c6aab5c39f73cca1a1649
Thisislearner <ak97plutonium@gmail.com>
2022-11-27 19:37:19 +0530

    changed display_drinks function
    separated the drinks_display dictionary from closed drinks count

--- display_creater.py
+++ display_creater.py
@@ -6,10 +6,20 @@ from resources import main_ingredients, snacks, sweeteners
 from menu import MENU
 
 
+# def closed_drinks():
+#     """this function counts the closed drinks in the menu."""
+#     closed_drink_count = 0
+#     for drink in MENU:
+#         for ingredient in MENU[drink]:
+#             if main_ingredients[ingredient] < MENU[drink][ingredient]:
+#                 closed_drink_count += 1
+#                 break
+#     return closed_drink_count
+
+
 def drink_display():
     """this function creates display for drinks"""
     display = {}  # hold all drinks with price.
-    closed_drink = 0  # to hold count closed drinks.
     drink_num = 1  # drinks number start from 1.
 
     for drink in MENU:
@@ -18,10 +28,9 @@ def drink_display():
         ingredients_list = MENU[drink]["ingredients"]
         for ingredient in ingredients_list:
             if ingredients_list[ingredient] > main_ingredients[ingredient]:
-                closed_drink += 1
                 drink_name = "Closed"
                 drink_price = "NaN"
                 break
         display[str(drink_num)] = [drink_name, drink_price]
         drink_num += 1
-    return [closed_drink, display]
+    return display
