d0240571bed45699c90bccd2a744e68068c8544f
Thisislearner <ak97plutonium@gmail.com>
2022-11-29 12:03:13 +0530

    display command changed to user_command
    this is done to make command look more general

--- command.py
+++ command.py
@@ -2,10 +2,10 @@
 # contains code for all the command for drinks, snacks and sweeteners
 ######################################################################
 
-def drink_commands(drinks_list):
+def user_commands(drinks_list):
     """this function takes commands for drinks."""
     user_input = input(f"please select from {drinks_list} : ")
     if user_input in drinks_list or user_input == "report" or user_input == 'x':
         return user_input
     else:
-        return drink_commands(drinks_list)
+        return user_commands(drinks_list)
