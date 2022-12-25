63855a18417d5ca6e11b14131fc17c698367652d
Thisislearner <ak97plutonium@gmail.com>
2022-12-25 11:07:37 +0530

    added off in command
    added off command in drinks to free x for canceling

--- command.py
+++ command.py
@@ -5,7 +5,7 @@
 def user_commands(drinks_list):
     """this function takes commands for drinks."""
     user_input = input(f"please select from {drinks_list} : ")
-    if user_input in drinks_list or user_input == "report" or user_input == 'x':
+    if user_input in drinks_list or user_input == "report" or user_input == 'x' or user_input == "off":
         return user_input
     else:
         return user_commands(drinks_list)
